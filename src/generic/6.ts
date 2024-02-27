efef9b09bf4a0187ae1acf7bfa1ad5ea8917538e
SnehaShisNasty <katirina18@gmail.com>
2024-02-27 21:03:22 +0000

    commit

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -12,12 +12,8 @@ type User = {
   surname: string;
   email: string;
   password: string;
-}
+};
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
-}
+function createOrUpdateUser(initialValues: Partial<User>) {}
 
-createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
-
-export {};
+createOrUpdateUser({ email: "user@mail.com", password: "password123" });
--- src/generic/8.ts
+++ src/generic/8.ts
@@ -2,6 +2,7 @@
   У вас є тип Form, який містить інформацію про форму, включаючи поле errors. 
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
+type Params = Omit<Form, "errors">;
 
 type Errors = {
   email?: string[];
@@ -17,8 +18,5 @@ type Form = {
   phone: string | null;
   errors: Errors;
 };
-
-// Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
 
 export {};
