145a8259e1c27925625d96cbed9686d8df6865b7
SnehaShisNasty <katirina18@gmail.com>
2024-02-28 18:28:00 +0000

    some changes

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,12 +1,15 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Comp {
+  title: string;
+}
 
 class Component<T> {
   constructor(public props: T) {}
 }
 
-class Page extends Component<{ title: string }> {
+class Page extends Component<Comp> {
   pageInfo() {
     console.log(this.props.title);
   }
